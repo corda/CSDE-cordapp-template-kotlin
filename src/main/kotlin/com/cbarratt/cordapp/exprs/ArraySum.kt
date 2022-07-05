ca3456d0921c9d0510812a193e56925f0648ce98
Chris Barratt <chris.barratt@r3.com>
2022-07-05 10:10:37 +0100

    Working get date-time, simple array sum, beginnings of more complex things

---
+++ src/main/kotlin/com/cbarratt/cordapp/exprs/ArraySum.kt
@@ -0,0 +1,149 @@
+package com.cbarratt.cordapp.exprs
+
+import net.corda.v5.application.flows.CordaInject
+import net.corda.v5.application.flows.FlowEngine
+import net.corda.v5.application.flows.RPCRequestData
+import net.corda.v5.application.flows.RPCStartableFlow
+import net.corda.v5.application.flows.SubFlow
+import net.corda.v5.application.flows.getRequestBodyAs
+import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.v5.base.annotations.Suspendable
+
+data class ArraySumArgs(
+    val data: IntArray? = IntArray(0)
+)
+
+data class IntArrayAndRange(val values: IntArray, val start: Int, val end: Int)
+
+data class ResultMessage(
+    val sum: Int
+)
+
+class ArraySumRPCFlowOnly : RPCStartableFlow {
+
+    @CordaInject
+    lateinit var flowEngine: FlowEngine
+
+    @CordaInject
+    lateinit var jsonMarshallingService: JsonMarshallingService
+
+    @Suspendable
+    override fun call(requestBody: RPCRequestData): String {
+        // val inputs = requestBody.getRequestBodyAs<InputMessage>(jsonMarshallingService)
+        val args = requestBody.getRequestBodyAs<ArraySumArgs>(jsonMarshallingService)
+
+        var retVal = 0
+        if (args.data != null) {
+            for (v in args.data) {
+                retVal += v
+            }
+            // retVal = flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(args.data, 0, args.data.size - 1)))
+        }
+        return jsonMarshallingService.format(ResultMessage(retVal))
+    }
+}
+
+
+class ArraySumNFlows : RPCStartableFlow {
+
+    @CordaInject
+    lateinit var flowEngine: FlowEngine
+
+    @CordaInject
+    lateinit var jsonMarshallingService: JsonMarshallingService
+
+    @Suspendable
+    override fun call(requestBody: RPCRequestData): String {
+        // val inputs = requestBody.getRequestBodyAs<InputMessage>(jsonMarshallingService)
+        val args = requestBody.getRequestBodyAs<ArraySumArgs>(jsonMarshallingService)
+
+        // Partition the array
+        var partSize = args.data.size / PARTITION_COUNT
+        if (partSize < MIN_ARRAY_LEN) {
+
+        }
+        var retVal = 0
+        if (args.data != null) {
+
+            retVal = flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(args.data, 0, args.data.size - 1)))
+        }
+        return jsonMarshallingService.format(ResultMessage(retVal))
+    }
+
+    companion object {
+        val PARTITION_COUNT = 12
+        val MIN_ARRAY_LEN = 2
+    }
+}
+
+
+class ArraySumSubFlow(val todo: IntArrayAndRange) : SubFlow<Int> {
+    @CordaInject
+    lateinit var flowEngine: FlowEngine
+
+    override fun call(): Int {
+        if (todo.start >= todo.end) return 0
+        var sum: Int = 0
+        val values = todo.values
+        if (values.size <= MIN_ARRAY_LEN) {
+            for (i in todo.start..todo.end) {
+                sum += values[i]
+            }
+        } else {
+            val mid = todo.end - todo.start / 2 + todo.start
+            sum = flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(values, todo.start, mid))) +
+                    flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(values, mid, todo.end)))
+        }
+        return sum
+    }
+    companion object {
+        val MIN_ARRAY_LEN = 2
+    }
+}
+
+/*
+class ArraySumNFlows : RPCStartableFlow {
+
+    @CordaInject
+    lateinit var flowEngine: FlowEngine
+
+    @CordaInject
+    lateinit var jsonMarshallingService: JsonMarshallingService
+
+    @Suspendable
+    override fun call(requestBody: RPCRequestData): String {
+        // val inputs = requestBody.getRequestBodyAs<InputMessage>(jsonMarshallingService)
+        val args = requestBody.getRequestBodyAs<ArraySumArgs>(jsonMarshallingService)
+
+        var retVal = 0
+        if (args.data != null) {
+            retVal = flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(args.data, 0, args.data.size - 1)))
+        }
+        return jsonMarshallingService.format(ResultMessage(retVal))
+    }
+}
+
+class ArraySumCrazySubFlow(val todo: IntArrayAndRange) : SubFlow<Int> {
+    @CordaInject
+    lateinit var flowEngine: FlowEngine
+
+    override fun call(): Int {
+        if (todo.start >= todo.end) return 0
+        var sum: Int = 0
+        val values = todo.values
+        if (values.size <= MIN_ARRAY_LEN) {
+            for (i in todo.start..todo.end) {
+                sum += values[i]
+            }
+        } else {
+            val mid = todo.end - todo.start / 2 + todo.start
+            sum = flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(values, todo.start, mid))) +
+                flowEngine.subFlow(ArraySumSubFlow(IntArrayAndRange(values, mid, todo.end)))
+        }
+        return sum
+    }
+    companion object {
+        val MIN_ARRAY_LEN = 2
+    }
+}
+*/
---
+++ src/main/kotlin/com/cbarratt/cordapp/exprs/GetDateAndTimeFlow.kt
@@ -0,0 +1,25 @@
+package com.cbarratt.cordapp.exprs
+
+import net.corda.v5.application.flows.CordaInject
+import net.corda.v5.application.flows.FlowEngine
+import net.corda.v5.application.flows.RPCRequestData
+import net.corda.v5.application.flows.RPCStartableFlow
+import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.v5.base.util.contextLogger
+import java.time.LocalDateTime
+
+class GetDateAndTimeFlow : RPCStartableFlow {
+    private companion object {
+        val log = contextLogger()
+    }
+
+    @CordaInject
+    lateinit var flowEngine: FlowEngine
+
+    @CordaInject
+    lateinit var jsonMarshallingService: JsonMarshallingService
+
+    override fun call(requestBody: RPCRequestData): String {
+        return jsonMarshallingService.format("The current time is:${LocalDateTime.now()}")
+    }
+}
