7a12b2fba56a99b8c7411295d91beda728ce3f8a
Chris Barratt <chris.barratt@r3.com>
2022-07-27 22:05:17 +0100

    Cpi generation, combinedworker jar unzipping task, Corda API ver extraction, etc

---
+++ tools/postgres-nonpersist-start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Start a clean postgres instance on docker, this will delete older instances.
+docker run --rm -p 5432:5432 --name postgresql -e POSTGRES_DB=cordacluster -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password postgres:latest
