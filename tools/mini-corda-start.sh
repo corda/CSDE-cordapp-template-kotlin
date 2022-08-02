b59a2b7fea7548e1ecb74cc4dfb59374d2ef2981
Chris Barratt <chris.barratt@r3.com>
2022-08-02 14:20:12 +0100

    Fix DB user, make consistent across commands

--- tools/mini-corda-start.sh
+++ tools/mini-corda-start.sh
@@ -1,9 +1,18 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash -x
 
 #COMBIWORKER_JAR=$(ls ~/.corda/corda5/working/corda-comb*.jar | tail -n 1)
-COMBIWORKER_JAR=$(ls ~/.corda/corda5/corda-comb*.jar | tail -n 1)
+COMBIWORKER_JAR=${1:-$(ls ~/.corda/corda5/corda-comb*.jar | tail -n 1)}
 
 echo "Running: $COMBIWORKER_JAR"
+
+#java \
+#   '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005' \
+#   -Dco.paralleluniverse.fibers.verifyInstrumentation=true \
+#   -jar $COMBIWORKER_JAR \
+#   --instanceId=0 -mbus.busType=DATABASE \
+#   -spassphrase=password -ssalt=salt -spassphrase=password -ssalt=salt \
+#   -ddatabase.user=user -ddatabase.pass=password \
+#   -ddatabase.jdbc.url=jdbc:postgresql://localhost:5432/cordacluster
 
 java \
    '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005' \
@@ -11,7 +20,15 @@ java \
    -jar $COMBIWORKER_JAR \
    --instanceId=0 -mbus.busType=DATABASE \
    -spassphrase=password -ssalt=salt -spassphrase=password -ssalt=salt \
-   -ddatabase.user=user -ddatabase.pass=password \
+   -ddatabase.user=postgres -ddatabase.pass=password \
    -ddatabase.jdbc.url=jdbc:postgresql://localhost:5432/cordacluster
 
 
+#java \
+#   '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005' \
+#   -Dco.paralleluniverse.fibers.verifyInstrumentation=true \
+#   -jar $COMBIWORKER_JAR \
+#   --instanceId=0 -mbus.busType=DATABASE \
+#   -spassphrase=password -ssalt=salt -spassphrase=password -ssalt=salt \
+#   -ddatabase.user=cordappdev -ddatabase.pass=password \
+#   -ddatabase.jdbc.url=jdbc:postgresql://localhost:5432/cordacluster
--- tools/postgres-nonpersist-start.sh
+++ tools/postgres-nonpersist-start.sh
@@ -2,3 +2,5 @@
 
 # Start a clean postgres instance on docker, this will delete older instances.
 docker run --rm -p 5432:5432 --name postgresql -e POSTGRES_DB=cordacluster -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password postgres:latest
+# docker run --rm -p 5432:5432 --name postgresql -e POSTGRES_DB=cordacluster -e POSTGRES_USER=cordappdev -e POSTGRES_PASSWORD=password postgres:latest
+
