0e85e486350882bcbfc5c1795c04e964bce1a368
Chris Barratt <chris.barratt@r3.com>
2022-08-24 19:09:57 +0100

    WIP changes to switching to the latest test-utils

--- src/test/kotlin/com/r3/examples/tests/ConcatFlowTest.kt
+++ src/test/kotlin/com/r3/examples/tests/ConcatFlowTest.kt
@@ -2,9 +2,11 @@ package com.r3.examples.tests
 
 import com.r3.examples.ConcatFlow
 import com.r3.examples.ConcatInputMessage
-import net.corda.testutils.FakeCorda
+import net.corda.testutils.CordaSim
+import net.corda.testutils.HoldingIdentity
 import net.corda.testutils.tools.CordaFlowChecker
-import net.corda.testutils.tools.RPCRequestDataMock
+import net.corda.testutils.tools.ResponderMock
+import net.corda.testutils.tools.ResponderMock
 import net.corda.v5.base.types.MemberX500Name
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
@@ -23,18 +25,27 @@ class ConcatFlowTest {
 
     @Test
     fun `ConcatFlow should concatenate the correct string to the input message`() {
-        val corda = FakeCorda()
-        corda.upload(eric, ConcatFlow::class.java)
+        val corda = CordaSim()
+        val node = corda.createVirtualNode(HoldingIdentity(eric), ConcatFlow::class.java)
         val messageText = "Suffix here->"
+        val response1 = node.callFlow(
+            RPCRequestDataWrapper("r1",
+                ConcatFlow::class.java,
+                ConcatInputMessage(messageText)
+            )
+        )
         // Stongly typed version to recommend.
+        /*
         val response1 = corda.invoke(
             eric,
-            RPCRequestDataMock.fromData(
+            ResponderMock.fromData(
                 "r1",
                 ConcatFlow::class.java,
                 ConcatInputMessage(messageText)
             )
         )
+
+         */
 
         println("response=$response1")
         assertThat(response1, `is`("{\"outText\":\"${messageText}${ConcatFlow.CONCAT_TEXT}\"}"))
