bf333a77605561c9c7d9bd1d7f4133c2d1eb335c
mattbradburyr3 <matthew.bradbury@r3.com>
2022-09-14 14:25:01 +0100

    first commit

---
+++ src/main/kotlin/net/cordapp/myfirstflow/MyFirstFlow.kt
@@ -0,0 +1,37 @@
+package net.cordapp.myfirstflow
+
+import net.corda.v5.application.flows.*
+import net.corda.v5.application.messaging.FlowSession
+import net.corda.v5.base.annotations.Suspendable
+import net.corda.v5.base.types.MemberX500Name
+import net.corda.v5.base.util.contextLogger
+
+
+class MyFirstFlowStartArgs(val counterparty: MemberX500Name, val message: String)
+
+@InitiatingFlow(protocol = "my-first-flow")
+class MyFirstFlow: RPCStartableFlow {
+
+    private companion object {
+        val log = contextLogger()
+    }
+@Suspendable
+    override fun call(requestBody: RPCRequestData): String {
+
+        log.info("MFF: a Log message")
+
+
+        return "Hello Alice"
+    }
+
+
+}
+
+@InitiatedBy(protocol = "my-first-flow")
+class MyFirstFlowResponder: ResponderFlow {
+
+    @Suspendable
+    override fun call(session: FlowSession) {
+
+    }
+}
