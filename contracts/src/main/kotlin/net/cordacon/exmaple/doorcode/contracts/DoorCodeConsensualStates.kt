c601dc6cdfec954212632ece906bb46d02b18274
Chris Barratt <chris.barratt@r3.com>
2022-12-19 16:33:41 +0000

    More work to make door code example usable

---
+++ contracts/src/main/kotlin/net/cordacon/exmaple/doorcode/contracts/DoorCodeConsensualStates.kt
@@ -0,0 +1,14 @@
+package net.cordacon.exmaple.doorcode.contracts
+
+import net.corda.v5.base.annotations.CordaSerializable
+import net.corda.v5.ledger.consensual.ConsensualState
+import net.corda.v5.ledger.consensual.transaction.ConsensualLedgerTransaction
+import java.security.PublicKey
+
+@CordaSerializable
+data class DoorCode(val code: String)
+
+@CordaSerializable
+class DoorCodeConsensualState(val code: DoorCode, override val participants: List<PublicKey>) : ConsensualState {
+    override fun verify(ledgerTransaction: ConsensualLedgerTransaction) {}
+}
