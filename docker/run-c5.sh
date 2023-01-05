298f083f8cabcfb68ce577a6ef9011e7eb8db394
Emil Gabrovski <emil.gabrovski@r3.com>
2023-01-05 13:46:11 +0200

    Docker-compose ... a long way to go ...

---
+++ docker/run-c5.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+java -version
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005 -DsecurityPolicyAllPermissions=true -Dco.paralleluniverse.fibers.verifyInstrumentation=true -jar ./corda-combined-worker.jar --instanceId=0 -mbus.busType=DATABASE -spassphrase=password -ssalt=salt -spassphrase=password -ssalt=salt -ddatabase.user=user -ddatabase.pass=password -ddatabase.jdbc.url=jdbc:postgresql://localhost:5432/cordacluster -ddatabase.jdbc.directory=./jdbcDrivers
