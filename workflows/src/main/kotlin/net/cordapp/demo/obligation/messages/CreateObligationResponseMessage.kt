6bbd46aa67fcdecfed3af1467025488083408287
Chris Barratt <chris.barratt@r3.com>
2023-01-11 12:24:10 +0000

    Remove unneeded UTXO demo apps and commented refs to internal repos

--- workflows/src/main/kotlin/net/cordapp/demo/obligation/messages/CreateObligationResponseMessage.kt
+++
@@ -1,6 +0,0 @@
-package net.cordapp.demo.obligation.messages
-
-import net.corda.v5.crypto.SecureHash
-import java.util.UUID
-
-data class CreateObligationResponseMessage(val transactionId: SecureHash, val obligationId: UUID)
