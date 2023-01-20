805a3790c9c45956e0bc9305822e14fb68d3daa0
Emil Gabrovski <emil.gabrovski@r3.com>
2023-01-20 10:17:30 +0200

    Fix the MyToken constructor from TokenState

--- CorDappUtxo/src/main/kotlin/com/r3/developers/csdetemplate/utxo/MyToken.kt
+++ CorDappUtxo/src/main/kotlin/com/r3/developers/csdetemplate/utxo/MyToken.kt
@@ -4,5 +4,5 @@ import net.corda.v5.base.types.MemberX500Name
 import java.util.*
 
 data class MyToken(val issuer: MemberX500Name, val owner: MemberX500Name, val amount: Int, val id: UUID) {
-    constructor(token: TokenState) : this(token.issuer.name, token.issuer.name, token.amount, token.id)
+    constructor(token: TokenState) : this(token.issuer.name, token.owner.name, token.amount, token.id)
 }
