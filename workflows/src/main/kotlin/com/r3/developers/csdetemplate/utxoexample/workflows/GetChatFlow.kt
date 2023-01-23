eddf61a1e5671a703e50c1433e8d91378f7aaa8c
mattbradburyr3 <matthew.bradbury@r3.com>
2023-01-23 11:27:09 +0000

    added commentary and some tidy up refactoring

--- workflows/src/main/kotlin/com/r3/developers/csdetemplate/utxoexample/workflows/GetChatFlow.kt
+++ workflows/src/main/kotlin/com/r3/developers/csdetemplate/utxoexample/workflows/GetChatFlow.kt
@@ -11,10 +11,13 @@ import net.corda.v5.ledger.utxo.StateAndRef
 import net.corda.v5.ledger.utxo.UtxoLedgerService
 import java.util.*
 
+// A class to hold the deserialized arguments required to start the flow.
 data class GetChatFlowArgs(val id: UUID, val numberOfRecords: Int)
 
-data class GetChatResponse(val messageFrom: String, val message: String)
+// A class to pair the messageFrom and message together.
+data class MessageAndSender(val messageFrom: String, val message: String)
 
+// See Chat CorDapp Design section of the getting started docs for a description of this flow.
 class GetChatFlow: RPCStartableFlow {
 
     private companion object {
@@ -24,44 +27,67 @@ class GetChatFlow: RPCStartableFlow {
     @CordaInject
     lateinit var jsonMarshallingService: JsonMarshallingService
 
+    // Injects the UtxoLedgerService to enable the flow to make use of the Ledger API.
     @CordaInject
     lateinit var ledgerService: UtxoLedgerService
 
     @Suspendable
     override fun call(requestBody: RPCRequestData): String {
 
+        log.info("GetChatFlow.call() called")
+
+        // Obtain the deserialized input arguments to the flow from the requestBody.
         val flowArgs = requestBody.getRequestBodyAs(jsonMarshallingService, GetChatFlowArgs::class.java)
 
+        // Look up the latest unconsumed ChatState with the given id.
+        // Note, this code brings all unconsumed states back, then filters them.
+        // This is an inefficient way to perform this operation when there are a large number of chats.
+        // Note, you will get this error if you input an id which has no corresponding ChatState (common error).
         val states = ledgerService.findUnconsumedStatesByType(ChatState::class.java)
         val state = states.singleOrNull {it.state.contractState.id == flowArgs.id}
             ?: throw Exception("did not find an unique ChatState")
 
+        // Calls resolveMessagesFromBackchain() which retrieves the chat history from the backchain.
         return jsonMarshallingService.format(resolveMessagesFromBackchain(state, flowArgs.numberOfRecords ))
     }
 
+    // resoveMessageFromBackchain() starts at the stateAndRef provided, which represents the unconsumed head of the
+    // backchain for this particular chat, then walks the chain backwards for the number of transaction specified in
+    // the numberOfRecords argument. For each transaction it adds the MessageAndSender representing the
+    // message and who sent it to a list which is then returned.
     @Suspendable
-    private fun resolveMessagesFromBackchain(stateAndRef: StateAndRef<ChatState>, numberOfRecords: Int): List<GetChatResponse>{
+    private fun resolveMessagesFromBackchain(stateAndRef: StateAndRef<ChatState>, numberOfRecords: Int): List<MessageAndSender>{
 
-        val messages = mutableListOf<GetChatResponse>()
+        // Set up a MutableList to collect the MessageAndSender(s)
+        val messages = mutableListOf<MessageAndSender>()
 
+        // Set up initial conditions for walking the backchain.
         var currentStateAndRef = stateAndRef
         var recordsToFetch = numberOfRecords
         var moreBackchain = true
 
+        // Continue to loop until the start of the backchain or enough records have been retrieved.
         while (moreBackchain) {
 
+            // Obtain the transaction id from the current StateAndRef and fetch the transaction from the vault.
             val transactionId = currentStateAndRef.ref.transactionHash
-
             val transaction = ledgerService.findLedgerTransaction(transactionId)
                 ?: throw Exception("Transaction $transactionId not found")
 
+            // Get the output state from the transaction and use it to create a MessageAndSender Object which
+            // is appended to the mutable list.
             val output = transaction.getOutputStates(ChatState::class.java).singleOrNull()
                 ?: throw Exception("Expecting one and only one ChatState output for transaction $transactionId")
-            messages.add(GetChatResponse(output.messageFrom.toString(), output.message))
+            messages.add(MessageAndSender(output.messageFrom.toString(), output.message))
+            // Decrement the number of records to fetch.
             recordsToFetch--
 
+            // Get the reference to the input states
             val inputStateAndRefs = transaction.inputStateAndRefs
 
+            // Check if there are no more input states (start of chain) or we have retrieved enough records.
+            // Check the transaction is not malformed by having too many input states.
+            // Set the currentStateAndRef to the input Stateand Ref, then repeat the loop.
             if (inputStateAndRefs.isEmpty() || recordsToFetch == 0) {
                 moreBackchain = false
             } else if (inputStateAndRefs.size > 1) {
@@ -71,6 +97,7 @@ class GetChatFlow: RPCStartableFlow {
                 currentStateAndRef = inputStateAndRefs.single() as StateAndRef<ChatState>
             }
         }
+     // Convert to an immutable List.
      return messages.toList()
     }
 }
