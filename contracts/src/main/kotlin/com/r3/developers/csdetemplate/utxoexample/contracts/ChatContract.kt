b764d9d2971cb3d30c178d23f3c96b3c902a9c53
mattbradburyr3 <matthew.bradbury@r3.com>
2023-01-28 15:48:23 +0000

    added TestContractFlow

--- contracts/src/main/kotlin/com/r3/developers/csdetemplate/utxoexample/contracts/ChatContract.kt
+++ contracts/src/main/kotlin/com/r3/developers/csdetemplate/utxoexample/contracts/ChatContract.kt
@@ -17,36 +17,40 @@ class ChatContract: Contract {
     override fun verify(transaction: UtxoLedgerTransaction) {
 
         // Ensures that there is only one command in the transaction
-        val command = transaction.commands.singleOrNull() ?: throw CordaRuntimeException("Require a single command ")
+        val command = transaction.commands.singleOrNull() ?: throw CordaRuntimeException("Requires a single command.")
 
         // Applies a universal constraint (applies to all transactions irrespective of command)
-        "The output state should have two and only two participants" using {
-            val output = transaction.outputContractStates.single() as ChatState
+        "The output state should have two and only two participants." using {
+            val output = transaction.outputContractStates.first() as ChatState
             output.participants.size== 2
         }
         // Switches case based on the command
         when(command) {
             // Rules applied only to transactions with the Create Command.
             is Create -> {
-                "When command is Create there should be no input states" using (transaction.inputContractStates.isEmpty())
-                "When command is Create there should be one and only one output state" using (transaction.outputContractStates.size == 1)
+                "When command is Create there should be no input states." using (transaction.inputContractStates.isEmpty())
+                "When command is Create there should be one and only one output state." using (transaction.outputContractStates.size == 1)
             }
             // Rules applied only to transactions with the Update Command.
             is Update -> {
-                "When command is Update there should be one and only one input states" using (transaction.inputContractStates.size == 1)
-                "When command is Update there should be one and only one output state" using (transaction.outputContractStates.size == 1)
+                "When command is Update there should be one and only one input states." using (transaction.inputContractStates.size == 1)
+                "When command is Update there should be one and only one output state." using (transaction.outputContractStates.size == 1)
+                // todo: fields that don't change test
+            }
+            else -> {
+                throw CordaRuntimeException("Command not allowed.")
             }
         }
     }
 
     // Helper function to allow writing constraints in the Corda 4 '"text" using (boolean)' style
     private infix fun String.using(expr: Boolean) {
-        if (!expr) throw IllegalArgumentException("Failed requirement: $this")
+        if (!expr) throw CordaRuntimeException("Failed requirement: $this")
     }
 
     // Helper function to allow writing constraints in '"text" using {lambda}' style where the last expression
     // in the lambda is a boolean.
     private infix fun String.using(expr: () -> Boolean) {
-        if (!expr.invoke()) throw IllegalArgumentException("Failed requirement: $this")
+        if (!expr.invoke()) throw CordaRuntimeException("Failed requirement: $this")
     }
 }
