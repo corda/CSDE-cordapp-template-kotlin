b1c6caf025aef78e697561a8dde114f7abb4785e
mattbradburyr3 <matthew.bradbury@r3.com>
2023-03-09 14:40:04 +0000

    clear review points

--- buildSrc/src/main/java/com/r3/csde/VNodesHelper.java
+++ buildSrc/src/main/java/com/r3/csde/VNodesHelper.java
@@ -1,6 +1,5 @@
 package com.r3.csde;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,7 +8,6 @@ import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -34,21 +32,26 @@ public class VNodesHelper {
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
-    // Entry point for setting up vnodes, called from the 5-vNodeSetup task in csde.gradle
-    public void vNodesSetup() throws IOException, CsdeException {
+    /**
+     * Entry point for setting up vnodes, called from the 5-vNodeSetup task in csde.gradle
+     */
+    public void vNodesSetup() throws CsdeException {
         List<VNode> requiredNodes = config.getVNodes();
         createVNodes(requiredNodes);
         registerVNodes(requiredNodes);
     }
 
-    // Creates vnodes specified in the config if they don't already exist.
-    private void createVNodes(List<VNode> nodes) throws IOException, CsdeException {
+    /**
+     * Creates vnodes specified in the config if they don't already exist.
+     * @param requiredNodes represents the list of VNodes as specified in the network Config json file (static-network-config.json)
+     */
+    private void createVNodes(List<VNode> requiredNodes) throws CsdeException {
 
         // Get existing Nodes.
         List<VirtualNodeInfoDTO> existingVNodes = getExistingNodes();
 
         // Check if each required vnode already exist, if not create it.
-        for (VNode vn : nodes) {
+        for (VNode vn : requiredNodes) {
             // Match on x500 and cpi name
             List<VirtualNodeInfoDTO> matches = existingVNodes.stream().filter(existing ->
                     existing.getHoldingIdentity().getX500Name().equals( vn.getX500Name()) &&
@@ -61,6 +64,10 @@ public class VNodesHelper {
         }
     }
 
+    /**
+     * Gets a list of the virtual nodes which have already been created.
+     * @return a list of the virtual nodes which have already been created.
+     */
     private List<VirtualNodeInfoDTO> getExistingNodes () throws CsdeException {
 
         HttpResponse<JsonNode> response = Unirest.get(pc.baseURL + "/api/v1/virtualnode")
@@ -78,7 +85,10 @@ public class VNodesHelper {
         }
     }
 
-    // Creates a vnode on the corda cluster from the VNode info.
+    /**
+     * Creates a vnode on the Corda cluster from the VNode info.
+     * @param vNode represents a virtual node using VNode Class.
+     */
     private void createVNode(VNode vNode) throws CsdeException {
 
         pc.out.println("Creating virtual node for "+ vNode.getX500Name());
@@ -97,7 +107,10 @@ public class VNodesHelper {
             throw new CsdeException("Creation of virtual node failed with response status: " + response.getStatus());
     }
 
-    // Reads the latest CPI checksums from file.
+    /**
+     * Reads the latest CPI checksums from file.
+     * @param vNode represents a virtual node using VNode Class.
+     */
     private String getCpiCheckSum(VNode vNode) throws CsdeException {
 
         try {
@@ -140,9 +153,11 @@ public class VNodesHelper {
         }
     }
 
-
-    // Checks if required vnodes have been registered and if not registers them.
-    private void registerVNodes(List<VNode> requiredNodes) throws JsonProcessingException, CsdeException {
+    /**
+     * Checks if the required virtual nodes have been registered and if not registers them.
+     * @param requiredNodes represents the list of VNodes as specified in the network Config json file (static-network-config.json)
+     */
+    private void registerVNodes(List<VNode> requiredNodes) throws CsdeException {
 
         // There appears to be a delay between the successful post /virtualnodes synchronous call and the
         // vnodes being returned in the GET /virtualnodes call. Putting a thread wait here as a quick fix
@@ -170,13 +185,19 @@ public class VNodesHelper {
             }
         }
     }
-    private void registerVnode( VNode vn, String shortHash) throws CsdeException {
 
-        pc.out.println("Registering vNode: " + vn.getX500Name() + " with shortHash: " + shortHash);
+    /**
+     * Registers a virtual node.
+     * @param vnode represents the vnode to be registered.
+     * @param shortHash the shortHash of the virtual node to register.
+     */
+    private void registerVnode(VNode vnode, String shortHash) throws CsdeException {
+
+        pc.out.println("Registering vNode: " + vnode.getX500Name() + " with shortHash: " + shortHash);
 
         // Configure the registration body (notary vs non notary)
         String registrationBody;
-        if (vn.getServiceX500Name() == null) {
+        if (vnode.getServiceX500Name() == null) {
             registrationBody =
                     "{ \"action\" : \"requestJoin\"," +
                             " \"context\" : {" +
@@ -187,7 +208,7 @@ public class VNodesHelper {
                             " \"context\" : {" +
                             " \"corda.key.scheme\" : \"CORDA.ECDSA.SECP256R1\", " +
                             " \"corda.roles.0\" : \"notary\", " +
-                            " \"corda.notary.service.name\" : \"" + vn.getServiceX500Name() + "\", " +
+                            " \"corda.notary.service.name\" : \"" + vnode.getServiceX500Name() + "\", " +
                             " \"corda.notary.service.plugin\" : \"net.corda.notary.NonValidatingNotary\" } }";
         }
 
@@ -208,7 +229,11 @@ public class VNodesHelper {
 
     }
 
-    // Checks if a virtual node with given shortHash has been registered
+    /**
+     * Checks if a virtual node with given shortHash has been registered
+     * @param shortHash shortHash of the node which is being checked.
+     * @return returns true if the vnode is registered
+     */
     private boolean checkVNodeIsRegistered(String shortHash) throws CsdeException {
 
         // Queries registration status for vnode.
@@ -241,7 +266,12 @@ public class VNodesHelper {
         }
     }
 
-    // polls for registration of a vnode
+    /**
+     * Polls for registration of a vnode
+     * @param shortHash short hash of the node which is being poled for
+     * @param duration the number of milliseconds before the pollign times out
+     * @param cooldown the number of milliseconds in between poll attempts.
+     */
     private void pollForRegistration(String shortHash, int duration, int cooldown) throws CsdeException {
 
         int timer = 0;
